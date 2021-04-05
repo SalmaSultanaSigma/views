52b22518e06bda2f01cfe55bbafbf75b800ee7cc
Salma Sultana <salma@deligram.com>
2021-04-05 19:02:42 +0600

    checkbox

--- app/src/main/java/com/example/viewcomponents/MainActivity.kt
+++ app/src/main/java/com/example/viewcomponents/MainActivity.kt
@@ -2,28 +2,32 @@ package com.example.viewcomponents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.FocusFinder
-import android.webkit.WebView
+import android.renderscript.ScriptGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.example.viewcomponents.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val b=findViewById<Button>(R.id.button)
-        val mytext=findViewById<TextView>(R.id.textView)
-        val edit=findViewById<EditText>(R.id.editTextNumber)
-        val edit2=findViewById<EditText>(R.id.editTextNumber2)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.checkbill.setOnClickListener {
+            var totalbill= 0
+            if(binding.pizza.isChecked)
+                totalbill+=3
+            if(binding.hotdog.isChecked)
+                totalbill+=1
+            if(binding.burger.isChecked)
+                totalbill+=2
 
-        b.setOnClickListener {
-            mytext.text="resuts: "+(edit.text.toString().toInt()+edit2.text.toString().toInt()).toString()
-            Toast.makeText(this, "result: "+mytext.text.toString(), Toast.LENGTH_SHORT).show()
-
+            binding.textView.setText("Total bill is: "+ totalbill.toString())
         }
     }
 }
